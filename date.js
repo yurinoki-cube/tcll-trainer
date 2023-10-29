f6870bf806a4da0999c3104d8f1209cd0e44701a
yurinoki-cube <63056828+yurinoki-cube@users.noreply.github.com>
2023-10-29 20:44:39 +0900

    Create date.js

---
+++ date.js
@@ -0,0 +1,27 @@
+//カウンター
+var count = 0;
+
+var cNumber = 0;
+var centers = [[true,true,true,true,true,true,true,true,true,true,true],
+               [true,true,true,true,true,true,true,true,true,true,true],
+               [true,true,true,true,true,true,true,true,true,true,true],
+               [true,true,true,true,true,true,true,true,true,true,true],
+               [true,true,true,true,true,true,true,true,true,true,true],
+               [true,true,true,true,true,true,true,true,true,true,true],
+               [true,true,true,true,true,true,true,true,true,true,true],
+               [true,true,true,true,true,true,true,true,true,true,true],
+               [true,true,true,true,true,true,true,true,true,true,true],
+               [true,true,true,true,true,true,true,true,true,true,true],
+               [true,true,true,true,true,true,true,true,true,true,true],
+               [true,true,true,true,true,true,true,true,true,true,true],
+               [true,true,true,true,true,true,true,true,true,true,true],
+               [true,true,true,true,true,true,true,true,true,true,true],
+               [true,true,true,true,true,true,true,true,true,true,true],
+               [true,true,true,true,true,true,true,true,true,true,true],
+               [true,true,true,true,true,true,true,true,true,true,true],
+               [true,true,true,true,true,true,true,true,true,true,true]];
+
+var centersID = ['center-pure','center-H','center-Z','center-Uh','center-O','center-Uv','center-TS','center-ZC','center-W','center-X','center-S'];
+
+var tclls = [['img-twist1-1','img-twist1-2','img-twist1-3','img-twist1-4','img-twist2-1','img-twist2-2','img-twist3-1','img-twist3-2','img-twist3-3','img-twist3-4','img-twist3-5','img-twist3-6','img-twist3-7','img-twist3-8','img-twist4-1','img-twist4-2','img-twist4-3','img-twist4-4'],
+['title-twist1-1','title-twist1-2','title-twist1-3','title-twist1-4','title-twist2-1','title-twist2-2','title-twist3-1','title-twist3-2','title-twist3-3','title-twist3-4','title-twist3-5','title-twist3-6','title-twist3-7','title-twist3-8','title-twist4-1','title-twist4-2','title-twist4-3','title-twist4-4']];
